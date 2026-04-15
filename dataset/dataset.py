3dc5f2367dc8957c3c34ef5d70db76232d10a592
AG-MI <62105984+AG-MI@users.noreply.github.com>
2026-04-15 20:47:47 +0200

    progress on the class distribution plotting

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import albumentations
 from collections import defaultdict
@@ -13,6 +14,7 @@ SIZE_IMG = 299
 
 class FedISIC2019_Dataset():
     fds = None
+    labels = None
 
     def __init__(self):
         #Loading Fed-ISIC2019 via flwr_datasets
@@ -27,6 +29,24 @@ class FedISIC2019_Dataset():
     def centralized_dataset(self):
         full_train = self.fds.load_split("train")
         full_test = self.fds.load_split("test")
+
+        self.labels = full_train.features["label"].names
         return full_train, full_test
+    
+    def plot_centralized_train_class_distribution(self):
+        full_train = self.centralized_dataset()
+        label_counter = [0, 0, 0, 0, 0, 0, 0, 0]
+        
+        for sample in full_train:
+            label_on_sample = sample["label"]
+            label_counter[label_on_sample] += 1
+        
+        
 
 
+
+
+
+full_train, full_test = FedISIC2019_Dataset().centralized_dataset()
+
+print(full_train[0]["label"])
