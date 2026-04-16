9d375066399cd3168dffb417d881e8208f696121
AG-MI <62105984+AG-MI@users.noreply.github.com>
2026-04-16 20:06:23 +0200

    Untested apply_oversampling_train_transform

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -16,8 +16,10 @@ class FedISIC2019_Dataset():
     fds = None
     labels = None
     labels_for_the_labels = ["mel", "mel-nev", "bcc", "ak", "bk", "df", "vl", "scc"]
+    seed = None
 
-    def __init__(self):
+    def __init__(self, seed: int):
+        self.seed = seed
         #Loading Fed-ISIC2019 via flwr_datasets
         self.fds = FederatedDataset(
             dataset="flwrlabs/fed-isic2019", 
@@ -50,6 +52,41 @@ class FedISIC2019_Dataset():
         plt.show()
 
         return
+    
+    def apply_oversampling_train_transform(self, pil_img):
+        if self.seed != None:
+            np.random.seed(self.seed)
+        
+        transform = albumentations.Compose([
+            albumentations.PadIfNeeded(min_height=SIZE_IMG, min_width=SIZE_IMG, border_mode=0, value=0),
+            albumentations.CenterCrop(height=SIZE_IMG, width=SIZE_IMG),
+            albumentations.RandomScale(0.07),
+            albumentations.RandomRotate90(),
+            albumentations.ShiftScaleRotate(),
+            albumentations.Normalize(normalization="min_max_per_channel")
+        ])
+
+        #Taking the Pillow formated image from the dataset and make it into a Numpy Array
+        img_np = np.array(pil_img)
+        
+        #Applying the transform
+        augmented = transform(image=img_np)["image"]
+
+        #Trasposing the image into a Pytorch-friendly tensor
+        tensor = torch.tensor(
+            np.transpose(augmented, (2, 0, 1)),
+            dtype=torch.float32
+        )
+
+        return tensor
+
+
+    
+
+    
+
+    
+    
 
 
 
